e9c988655b3052af54388288c4ff3ee1fa8350ed
Jacques Benzakein <Jackbenzakein@gmail.com>
2019-10-07 18:08:11 +0300

    Started implementing the isbn search view in the addBookEntryVC, including text field and submit button (though the button doesn't do much yet)

--- The Library/AddBookEntryVC.swift
+++ The Library/AddBookEntryVC.swift
@@ -14,13 +14,18 @@ class AddBookEntryVC: UIViewController {
     
     var lastSelected: Int = 0
     
+    let ISBNField: UITextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         //setupSegmentedControl()
-        displayISBNView()
         entryModeSwitcher.addTarget(self, action: #selector(switcherWasTapped), for: .valueChanged)
+    }
+    
+    override func viewSafeAreaInsetsDidChange() {
+        displayISBNView()
     }
     
     
@@ -45,10 +50,23 @@ class AddBookEntryVC: UIViewController {
             }
         }
         
-        let testLabel = UILabel()
-        testLabel.text = "ISBN test"
-        testLabel.frame = CGRect(x: 0, y: view.frame.height/2, width: view.frame.width, height: 70)
-        view.addSubview(testLabel)
+        ISBNField.textAlignment = .center
+        ISBNField.placeholder = "10- or 13-digit ISBN"
+        ISBNField.frame = CGRect(x: 20, y: view.safeAreaInsets.top+89, width: view.frame.width-40, height: 50)
+        //ISBNField.topAnchor.constraint(equalTo: entryModeSwitcher.bottomAnchor).isActive = true
+        view.addSubview(ISBNField)
+        
+        let submitISBNButton = UIButton()
+        submitISBNButton.setTitle("Look up ISBN", for: .normal)
+        submitISBNButton.setTitleColor(.blue, for: .normal)
+        
+        submitISBNButton.setTitle("Why you still holding the button?", for: .highlighted)
+        submitISBNButton.setTitleColor(.blue, for: .highlighted)
+        
+        submitISBNButton.frame = CGRect(x: view.frame.width/2 - 75, y: view.safeAreaInsets.top+89+50, width: 150, height: 40)
+        
+        submitISBNButton.addTarget(self, action: #selector(submitISBN), for: .touchUpInside)
+        view.addSubview(submitISBNButton)
     }
     
     func displayManualView(){
@@ -64,12 +82,21 @@ class AddBookEntryVC: UIViewController {
         view.addSubview(testLabel)
     }
     
+    @objc func submitISBN(){
+        let sanitized = sanitize(ISBNField.text ?? "")
+        print("Fetching book data for ISBN \(sanitized)...")
+    }
+    
+    func sanitize(_ isbn: String) -> String {
+            return isbn
+    }
+    
     func setupSegmentedControl(){
         let sc = UISegmentedControl()
         sc.insertSegment(withTitle: "ISBN", at: 0, animated: false)
         sc.insertSegment(withTitle: "Manual", at: 1, animated: false)
         
-        sc.frame = CGRect(x: 0, y: 20, width: view.frame.width, height: 40)
+        sc.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 40)
         
         view.addSubview(sc)
     }
